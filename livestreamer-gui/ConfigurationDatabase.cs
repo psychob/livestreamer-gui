65f47c2ffea94a06e9a29d02fae3997bb9af2076
Andrzej Budzanowski <psychob.pl@gmail.com>
2016-01-14 18:14:05 +0100

    Dodanie klasy ConfigurationDatabaseProxy

--- livestreamer-gui/ConfigurationDatabase.cs
+++ livestreamer-gui/ConfigurationDatabase.cs
@@ -210,4 +210,71 @@ namespace livestreamer_gui
             throw new KeyNotFoundException();
         }
     }
+
+    class ConfigurationDatabaseProxy
+    {
+        private ConfigurationDatabase innerDatabase;
+        private string prefix;
+
+        public ConfigurationDatabaseProxy( ConfigurationDatabase cdb, string prefix )
+        {
+            innerDatabase = cdb;
+            this.prefix = prefix;
+        }
+
+        public void SetString(string key, string value)
+        {
+            innerDatabase.SetString(prefix + "." + key, value);
+        }
+
+        public string GetString(string key, string def = "")
+        {
+            return innerDatabase.GetString(prefix + "." + key, def);
+        }
+
+        public void SetInt(string key, int value)
+        {
+            innerDatabase.SetInt(prefix + "." + key, value);
+        }
+
+        public int GetInt(string key, int def = 0)
+        {
+            return innerDatabase.GetInt(prefix + "." + key);
+        }
+
+        public void SetBoolean(string key, bool b)
+        {
+            innerDatabase.SetBoolean(prefix + "." + key, b);
+        }
+
+        public bool GetBoolean(string key, bool def = false)
+        {
+            return innerDatabase.GetBoolean(prefix + "." + key, def);
+        }
+
+        public void UpdateAutocomplete(string key, string url)
+        {
+            innerDatabase.UpdateAutocomplete(prefix + "." + key, url);
+        }
+
+        public bool RemoveAutocomplete(string key, string url)
+        {
+            return innerDatabase.RemoveAutocomplete(prefix + "." + key, url);
+        }
+
+        public List<ConfigurationDatabase.AutocompleteData> GetAutocomplete(string key)
+        {
+            return innerDatabase.GetAutocomplete(prefix + "." + key);
+        }
+
+        public void SetListOfStrings(string key, List<string> list)
+        {
+            innerDatabase.SetListOfStrings(prefix + "." + key, list);
+        }
+
+        public List<string> GetListOfStrings(string key)
+        {
+            return innerDatabase.GetListOfStrings(prefix + "." + key);
+        }
+    }
 }
