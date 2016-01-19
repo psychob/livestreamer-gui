90e835046053aef06ee2ceb4775d0ab78fca9589
Andrzej Budzanowski <psychob.pl@gmail.com>
2016-01-19 16:29:43 +0100

    Dodanie nowych metod rozszerzeń i aktualizacja starych

--- livestreamer-gui/ExtenstionMethods.cs
+++ livestreamer-gui/ExtenstionMethods.cs
@@ -8,19 +8,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace livestreamer_gui
 {
     static class ExtenstionMethods
     {
-        public static void Update<T>(this List<T> lst, Predicate<T> pred,
+        public static int Update<T>(this List<T> lst, Predicate<T> pred,
             Func<T, T> updater)
         {
+            int count = 0;
+
             for (int it = 0; it < lst.Count; ++it)
             {
                 if (pred(lst[it]))
+                {
                     lst[it] = updater(lst[it]);
+                    count++;
+                }
             }
+
+            return count;
         }
 
         public static int RemoveIf<T>(this List<T> lst, Predicate<T> pred)
@@ -38,6 +46,52 @@ namespace livestreamer_gui
             }
 
             return ret;
+        }
+
+        public static T[] Rewrite<T,U>(this List<U> lst, Func<U, T> rewriter)
+        {
+            if (lst.Count == 0)
+                return null;
+
+            T[] rarr = new T[lst.Count];
+
+            for (int it = 0; it < lst.Count; ++it)
+                rarr[it] = rewriter(lst[it]);
+
+            return rarr;
+        }
+
+        public static void WriteTag(this XmlWriter xml, string tagname,
+            string value, Dictionary<string, string> attributes = null)
+        {
+            xml.WriteStartElement(tagname);
+
+            if ( attributes != null)
+            {
+                foreach (var it in attributes)
+                    xml.WriteAttributeString(it.Key, it.Value);
+            }
+
+            xml.WriteString(value);
+
+            xml.WriteEndElement();
+        }
+
+        public static void StartTag(this XmlWriter xml, string name,
+            Dictionary<string, string> attributes = null)
+        {
+            xml.WriteStartElement(name);
+
+            if (attributes != null)
+            {
+                foreach (var it in attributes)
+                    xml.WriteAttributeString(it.Key, it.Value);
+            }
+        }
+
+        public static void EndTag(this XmlWriter xml)
+        {
+            xml.WriteEndElement();
         }
     }
 }
