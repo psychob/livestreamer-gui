12bb5dd90f29b0d4409c60ec883f4bc65aea7e4f
Andrzej Budzanowski <psychob.pl@gmail.com>
2016-01-19 16:29:23 +0100

    Aktualizacja configurationdatabase, configurationdatabaseproxy i dodanie ConfigurationConstants

--- livestreamer-gui/ConfigurationDatabase.cs
+++ livestreamer-gui/ConfigurationDatabase.cs
@@ -8,10 +8,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace livestreamer_gui
 {
-    class ConfigurationDatabase
+    public static class ConfigurationConstants
+    {
+        public const string LivestreamerPath = "core.livestreamer.path";
+        public const string LivestreamerLogLevel = "core.livestreamer.log-level";
+        public const string PlayerPath = "core.player.path";
+        public const string RetryStream = "core.stream.retry";
+        public const string RetryStreamAttempts = "core.stream.attempts";
+        public const string RetryStreamDelay = "core.stream.delay";
+        public const string LivestreamerHideConsole = "core.livestreamer.hide-console";
+        public const string PlayerGenerateVLCMetadata = "core.player.vlc";
+        public const string ApiInternetAccess = "core.api.internet";
+        public const string Autocomplete = "core.autocomplete";
+    }
+
+    public class ConfigurationDatabase
     {
         public struct ConfigValue
         {
@@ -76,7 +91,7 @@ namespace livestreamer_gui
             }
         }
 
-        private Dictionary<string, ConfigValue> values;
+        private Dictionary<string, ConfigValue> values = new Dictionary<string, ConfigValue>();
 
         public void SetString(string key, string value)
         {
@@ -138,7 +153,7 @@ namespace livestreamer_gui
                 return def;
         }
 
-        public void UpdateAutocomplete(string key, string url)
+        public void UpdateAutocomplete(string key, string url, int count = -1)
         {
             if (values.ContainsKey(key))
             {
@@ -147,8 +162,11 @@ namespace livestreamer_gui
                 if (obj.boxedType == ConfigValue.Type.AutocompleteList)
                 {
                     var acl = (List<AutocompleteData>)obj.boxedValue;
-                    acl.Update(p => p.url == url,
-                        upd => new AutocompleteData(upd.url, upd.count));
+                    int updated = acl.Update(p => p.url == url,
+                        upd => new AutocompleteData(upd.url, count == -1 ? upd.count + 1 : count));
+
+                    if (updated == 0)
+                        acl.Add(new AutocompleteData(url, count == -1 ? 1 : count));
                 }
                 else
                     throw new Exception("bad type");
@@ -156,7 +174,7 @@ namespace livestreamer_gui
             else
             {
                 List<AutocompleteData> lst = new List<AutocompleteData>();
-                lst.Add(new AutocompleteData(url));
+                lst.Add(new AutocompleteData(url, count == -1 ? 1 : count));
                 values[key] = new ConfigValue(lst);
             }
         }
@@ -189,7 +207,12 @@ namespace livestreamer_gui
                     return (List<AutocompleteData>)obj.boxedValue;
             }
 
-            throw new KeyNotFoundException();
+            return null;
+        }
+
+        public void SetAutocomplete(string newKey, List<AutocompleteData> ad)
+        {
+            values[newKey] = new ConfigValue(ad);
         }
 
         public void SetListOfStrings(string key, List<string> list)
@@ -209,9 +232,134 @@ namespace livestreamer_gui
 
             throw new KeyNotFoundException();
         }
+
+        public void LoadConfiguration(string file)
+        {
+            if (!System.IO.File.Exists(file))
+                return;
+
+            XmlReaderSettings xrs = new XmlReaderSettings();
+            xrs.CloseInput = true;
+
+            using (XmlReader xmlr = XmlReader.Create(file, xrs))
+            {
+                while (xmlr.Read())
+                {
+                    if (xmlr.NodeType == XmlNodeType.Element)
+                    {
+                        string nodeName = xmlr.GetAttribute("name");
+
+                        switch (xmlr.Name)
+                        {
+                            case "boolean":
+                                SetBoolean(nodeName, xmlr.ReadString() == "y");
+                                break;
+
+                            case "integer":
+                                SetInt(nodeName, int.Parse(xmlr.ReadString()));
+                                break;
+
+                            case "string":
+                                SetString(nodeName, xmlr.ReadString());
+                                break;
+
+                            case "autocomplete":
+                                xmlr.ReadToDescendant("entry");
+
+                                do
+                                {
+                                    int c = int.Parse(xmlr.GetAttribute("count"));
+                                    string url = xmlr.ReadString();
+
+                                    UpdateAutocomplete(nodeName, url, c);
+                                } while (xmlr.ReadToNextSibling("entry"));
+                                break;
+
+                            case "values":
+                            default:
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void SaveConfiguration(string file)
+        {
+            XmlWriterSettings xmls = new XmlWriterSettings();
+            xmls.CloseOutput = true;
+            xmls.Encoding = Encoding.UTF8;
+            xmls.Indent = true;
+            xmls.IndentChars = "\t";
+
+            using (XmlWriter xmlw = XmlWriter.Create(file, xmls))
+            {
+                xmlw.WriteStartDocument(true);
+                xmlw.WriteStartElement("values");
+
+                foreach (var it in values)
+                {
+                    switch (it.Value.boxedType)
+                    {
+                        case ConfigValue.Type.Boolean:
+                            xmlw.WriteTag("boolean", ((bool)it.Value.boxedValue) ? "y" : "f",
+                                new Dictionary<string, string>{
+                                    { "name", it.Key }
+                                });
+                            break;
+
+                        case ConfigValue.Type.Integer:
+                            xmlw.WriteTag("integer", ((int)it.Value.boxedValue).ToString(),
+                                new Dictionary<string, string>{
+                                    { "name", it.Key }
+                                });
+                            break;
+
+                        case ConfigValue.Type.String:
+                            xmlw.WriteTag("string", (string)it.Value.boxedValue,
+                                new Dictionary<string, string>{
+                                    { "name", it.Key }
+                                });
+                            break;
+
+                        case ConfigValue.Type.ListOfStrings:
+                            xmlw.StartTag("stringlist",
+                                new Dictionary<string, string>{
+                                    { "name", it.Key }
+                                });
+                            List<string> lst = (List<string>)it.Value.boxedValue;
+
+                            foreach (var jt in lst)
+                                xmlw.WriteTag("string", jt);
+
+                            xmlw.EndTag();
+                            break;
+
+                        case ConfigValue.Type.AutocompleteList:
+                            xmlw.StartTag("autocomplete",
+                                new Dictionary<string, string>{
+                                    { "name", it.Key }
+                                });
+                            List<AutocompleteData> alst = (List<AutocompleteData>)it.Value.boxedValue;
+
+                            foreach (var jt in alst)
+                                xmlw.WriteTag("entry", jt.url,
+                                    new Dictionary<string, string>{
+                                        { "count", jt.count.ToString() }
+                                    });
+
+                            xmlw.EndTag();
+                            break;
+                    }
+                }
+
+                xmlw.WriteEndElement();
+                xmlw.WriteEndDocument();
+            }
+        }
     }
 
-    class ConfigurationDatabaseProxy
+    public class ConfigurationDatabaseProxy
     {
         private ConfigurationDatabase innerDatabase;
         private string prefix;
@@ -222,59 +370,84 @@ namespace livestreamer_gui
             this.prefix = prefix;
         }
 
-        public void SetString(string key, string value)
+        public void SetString(string key, string value, bool withPrefix = true)
         {
-            innerDatabase.SetString(prefix + "." + key, value);
+            string newKey = withPrefix ? prefix + "." + key : key;
+
+            innerDatabase.SetString(newKey, value);
         }
 
-        public string GetString(string key, string def = "")
+        public string GetString(string key, string def = "",
+            bool withPrefix = true)
         {
-            return innerDatabase.GetString(prefix + "." + key, def);
+            string newKey = withPrefix ? prefix + "." + key : key;
+
+            return innerDatabase.GetString(newKey, def);
         }
 
-        public void SetInt(string key, int value)
+        public void SetInt(string key, int value, bool withPrefix = true)
         {
-            innerDatabase.SetInt(prefix + "." + key, value);
+            string newKey = withPrefix ? prefix + "." + key : key;
+            innerDatabase.SetInt(newKey, value);
         }
 
-        public int GetInt(string key, int def = 0)
+        public int GetInt(string key, int def = 0, bool withPrefix = true)
         {
-            return innerDatabase.GetInt(prefix + "." + key);
+            string newKey = withPrefix ? prefix + "." + key : key;
+            return innerDatabase.GetInt(newKey);
         }
 
-        public void SetBoolean(string key, bool b)
+        public void SetBoolean(string key, bool b, bool withPrefix = true)
         {
-            innerDatabase.SetBoolean(prefix + "." + key, b);
+            string newKey = withPrefix ? prefix + "." + key : key;
+            innerDatabase.SetBoolean(newKey, b);
         }
 
-        public bool GetBoolean(string key, bool def = false)
+        public bool GetBoolean(string key, bool def = false,
+            bool withPrefix = true)
         {
-            return innerDatabase.GetBoolean(prefix + "." + key, def);
+            string newKey = withPrefix ? prefix + "." + key : key;
+            return innerDatabase.GetBoolean(newKey, def);
         }
 
-        public void UpdateAutocomplete(string key, string url)
+        public void UpdateAutocomplete(string key, string url,
+            bool withPrefix = true)
         {
-            innerDatabase.UpdateAutocomplete(prefix + "." + key, url);
+            string newKey = withPrefix ? prefix + "." + key : key;
+            innerDatabase.UpdateAutocomplete(newKey, url);
         }
 
-        public bool RemoveAutocomplete(string key, string url)
+        public bool RemoveAutocomplete(string key, string url,
+            bool withPrefix = true)
         {
-            return innerDatabase.RemoveAutocomplete(prefix + "." + key, url);
+            string newKey = withPrefix ? prefix + "." + key : key;
+            return innerDatabase.RemoveAutocomplete(newKey, url);
         }
 
-        public List<ConfigurationDatabase.AutocompleteData> GetAutocomplete(string key)
+        public List<ConfigurationDatabase.AutocompleteData>
+            GetAutocomplete(string key, bool withPrefix = true)
         {
-            return innerDatabase.GetAutocomplete(prefix + "." + key);
+            string newKey = withPrefix ? prefix + "." + key : key;
+            return innerDatabase.GetAutocomplete(newKey);
         }
 
-        public void SetListOfStrings(string key, List<string> list)
+        public void SetListOfStrings(string key, List<string> list,
+            bool withPrefix = true)
         {
-            innerDatabase.SetListOfStrings(prefix + "." + key, list);
+            string newKey = withPrefix ? prefix + "." + key : key;
+            innerDatabase.SetListOfStrings(newKey, list);
         }
 
-        public List<string> GetListOfStrings(string key)
+        public List<string> GetListOfStrings(string key, bool withPrefix = true)
         {
-            return innerDatabase.GetListOfStrings(prefix + "." + key);
+            string newKey = withPrefix ? prefix + "." + key : key;
+            return innerDatabase.GetListOfStrings(newKey);
+        }
+
+        public void SetAutocomplete(string key, List<ConfigurationDatabase.AutocompleteData> ad, bool withPrefix = true)
+        {
+            string newKey = withPrefix ? prefix + "." + key : key;
+            innerDatabase.SetAutocomplete(newKey, ad);
         }
     }
 }
