8d180febb857846c097bd63d0b558702ebf89379
Andrzej Budzanowski <psychob.pl@gmail.com>
2016-01-19 16:28:29 +0100

    Dodanie nowego PluginAPI, który zastępuje WebsiteAPI

---
+++ livestreamer-gui/PluginAPI.cs
@@ -0,0 +1,52 @@
+﻿//
+// livestreamer-gui
+// (c) 2016 Andrzej Budzanowski <psychob.pl@gmail.com>
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace livestreamer_gui
+{
+    public struct StreamInfo
+    {
+        public List<string> Quality;
+        public string Title;
+        public string Author;
+        public string CanonicalUrl;
+    }
+
+    public struct InitData
+    {
+        public static class Event
+        {
+            public delegate void RunLiveStreamer();
+            public delegate void TabUpdated(string id);
+        }
+
+        public ConfigurationDatabaseProxy Config;
+        public System.Windows.Forms.TabControl Control;
+        public Event.RunLiveStreamer RunLiveStreamer;
+        public Event.TabUpdated TabUpdated;
+    }
+
+    interface PluginAPI
+    {
+        bool Owns(Uri url);
+
+        string GetCanonicalUrl();
+
+        StreamInfo GetVideoMedatada();
+
+        string GetPluginId();
+
+        void InitTab(InitData data);
+
+        void StreamStarted();
+
+        void ShutDown();
+    }
+}
